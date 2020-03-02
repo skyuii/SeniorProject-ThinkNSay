0e5a2a2e0a7d30b1e6364bdd4addf92dee53e3b9
skyuii <44127249+skyuii@users.noreply.github.com>
2020-03-02 19:03:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/seniorproject_thinknsay/SelectActivity.java
@@ -0,0 +1,14 @@
+package com.example.seniorproject_thinknsay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
