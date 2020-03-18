8458a8f1972ccbdc5fe08ba28d23cf8539cf4798
skyuii <44127249+skyuii@users.noreply.github.com>
2020-03-18 15:23:30 +0700

    new screen activity

--- app/src/main/java/com/example/seniorproject_thinknsay/ScoreScreen.java
+++ app/src/main/java/com/example/seniorproject_thinknsay/ScoreScreen.java
@@ -2,7 +2,10 @@ package com.example.seniorproject_thinknsay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ScoreScreen extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class ScoreScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_screen);
+
+        ImageButton next_button = findViewById(R.id.next_button);
+
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBackStart();
+            }
+        });
+    }
+
+    public void openBackStart(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
