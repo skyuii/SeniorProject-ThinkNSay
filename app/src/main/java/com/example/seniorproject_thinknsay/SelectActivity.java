00fb35e5ffdc9b84301fbd3bebb82f5415c66a32
skyuii <44127249+skyuii@users.noreply.github.com>
2020-03-18 15:38:41 +0700

    new screen activity

--- app/src/main/java/com/example/seniorproject_thinknsay/SelectActivity.java
+++ app/src/main/java/com/example/seniorproject_thinknsay/SelectActivity.java
@@ -2,7 +2,11 @@ package com.example.seniorproject_thinknsay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        ImageButton backward_button = findViewById(R.id.button_backward);
+        ImageButton forward_button = findViewById(R.id.button_forward);
+
+        backward_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeScreen();
+            }
+        });
+
+        forward_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayScreen();
+            }
+        });
+    }
+
+    public void openHomeScreen(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlayScreen(){
+        Intent intent = new Intent(this,PlayScreen.class);
+        startActivity(intent);
     }
 }
